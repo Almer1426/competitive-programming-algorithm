508280bac780c52ea8b6f44c07aa887a51b360b0
Almer1426 <almerajivalentino74@gmail.com>
2025-09-16 18:38:23 +0700

    feat: menambahkan Sparse_Table

---
+++ Sparse_Table_Min.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> a({1, 3, 3, 1, 2, 6});
+    int n = a.size();
+
+    vector<int> lg(n + 1, 0);
+    for(int i = 2; i <= n; i++) lg[i] = lg[i / 2] + 1;
+
+    vector<vector<int>> st(25 + 1, vector<int>(n)); for(int i = 0; i < n; i++) st[0][i] = a[i];
+    for(int i = 1; i <= 25; i++) for(int j = 0; j + (1 << i) <= n; j++) {
+        st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+    }
+
+    auto query = [&](int l, int r) {
+        int i = lg[r - l + 1];
+        return min(st[i][l], st[i][r - (1 << i) + 1]);
+    };
+
+    cout << ">> ";
+    cout << query(3 - 1, 4 - 1) << endl;
+
+    return 0;
+}
